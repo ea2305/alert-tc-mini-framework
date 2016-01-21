11a94fabe693e90213d1f73ad7ea6eb53720ed62
Elihu Cruz <ea_2305@hotmail.com>
2016-01-20 19:08:21 -0600

    implementacion de objeto

--- js/notify-tc.js
+++ js/notify-tc.js
@@ -1,79 +1,99 @@
-//Principal object Alert_tc
-Alert_tc = function(title){
+/*
+ * Version 1.4
+ */
 
-  this.title = title || "";
-  this.sizeX = 400;
-  this.sizeY = 150;
-  this.timeIn = 550;
-  this.timeOut = 400;
+(function () {
+    //Principal object Alert_tc
+    Alert_tc = function (title) {
 
-  var translate_start = {
-      width: this.sizeX,
-      opacity : 1
-  };
+        this.title = title || "";
+        this.sizeX = 400;
+        this.sizeY = 150;
+        this.timeIn = 300;
+        this.timeOut = 300;
 
-  var translate_end = {
-      width: 0,
-      right : 0,
-      opacity : 0
-  };
+        var translate_start = {
+            width: this.sizeX,
+            opacity: 1
+        };
 
-  this.setTitle = function(title){
-      this.title = title;
-  }
+        var translate_end = {
+            width: 0,
+            opacity: 0
+        };
 
-  this.error = function(title,content){
-      var timeLimit = 5000;
-      title = (this.title != '') ? this.title : title;
-      $('body').append(template(title,false));
-      $('.alert-notify').animate(translate_start,this.timeIn, function(){
+        this.setTitle = function (title) {
+            this.title = title;
+        }
 
-          setTimeout(function(){
+        this.error = function (title, position) {
+            var intervalClose;
+            var timeLimit = 5000;
+            title = (this.title != '') ? this.title : title;
 
-              $('.alert-notify').animate(translate_end,this.timeOut, function(){
-              //Eliminate element
-                  $('.alert-notify').remove();
-              })
-          }, timeLimit);
+            $('body').append(template(title, false));
+            $('.alert-notify').animate(translate_start, this.timeIn, function () {
 
-          $("div").click(function (e){
-              $('.alert-notify').animate(translate_end,this.timeOut, function(){
-              //Eliminate element
-                  $('.alert-notify').remove();
-              })
-          });
-    });
-  }
+                intervalClose = window.setTimeout(function () {
 
-  //Show alert when the call is correct
-  this.success = function(title,content){
-      var timeLimit = 5000;
-      title = (this.title != '') ? this.title : title;
-      $('body').append(template(title,true));
-      $('.alert-notify').animate(translate_start,this.timeIn, function(){
+                    $('.alert-notify').animate(translate_end, this.timeOut, function () {
+                        //Eliminate element
+                        $('.alert-notify').remove();
+                        window.clearTimeout(intervalClose);
+                    })
+                }, timeLimit);
 
-          setTimeout(function(){
-              $('.alert-notify').animate(translate_end,this.timeOut, function(){
-              //Eliminate element
-                  $('.alert-notify').remove();
-              })
-          }, timeLimit);
 
-          $("div").click(function (e){
-              $('.alert-notify').animate(translate_end,this.timeOut, function(){
-              //Eliminate element
-                  $('.alert-notify').remove();
-              })
-          });
-    });
-  }
+                $(".alert-notify").click(function (e) {
 
-  template = function(title,mode){
-    //Select style
-    var selected = (mode) ? "correct-notify":"incorrect-notify";
-    var format = "<div class='" + selected + " alert-notify'>" +
-        "<h2 id='title'>" + title + "</h2>" +
-      "</div>";
-    return format;
-  }
-}
+                    $('.alert-notify').animate(translate_end, this.timeOut, function () {
+
+                        $('.alert-notify').remove();
+                        window.clearTimeout(intervalClose);
+                    })
+                });
+            });
+        }
+
+        //Show alert when the call is correct
+        this.success = function (title, position) {
+            var intervalClose;
+            var timeLimit = 5000;
+            title = (this.title != '') ? this.title : title;
+            $('body').append(template(title, true));
+            $('.alert-notify').animate(translate_start, this.timeIn, function () {
+
+                intervalClose = window.setTimeout(function () {
+
+                    $('.alert-notify').animate(translate_end, this.timeOut, function () {
+                        //Eliminate element
+                        $('.alert-notify').remove();
+                        window.clearTimeout(intervalClose);
+                    })
+                }, timeLimit);
+
+
+                $(".alert-notify").click(function (e) {
+
+                    $('.alert-notify').animate(translate_end, this.timeOut, function () {
+
+                        $('.alert-notify').remove();
+                        window.clearTimeout(intervalClose);
+                    })
+                });
+            });
+        }
+
+        template = function (title, mode) {
+            //Select style
+
+
+            var selected = (mode) ? "correct-notify" : "incorrect-notify";
+            var format = "<div class='" + selected + " alert-notify'>" +
+                "<h2 id='title'>" + title + "</h2>" +
+              "</div>";
+            return format;
+        }
+    }
+    alertify_tc = new Alert_tc();
+})(this)
