060629b47be4c86d0faf590ca1fc3ce6e7182eeb
Elihu Cruz <ea_2305@hotmail.com>
2016-01-28 17:02:58 -0600

    fix object to redimention

--- js/notify-tc.js
+++ js/notify-tc.js
@@ -1,7 +1,3 @@
-/*
- * Version 1.4
- */
-
 (function () {
     //Principal object Alert_tc
     Alert_tc = function (title) {
@@ -9,11 +5,33 @@
         this.title = title || "";
         this.sizeX = 400;
         this.sizeY = 150;
+
+        //Changes in sizes
+        this.sizeY_small = 100;
+        this.sizeY_medium = 200;
+
         this.timeIn = 300;
         this.timeOut = 300;
 
+/*
         var translate_start = {
             width: this.sizeX,
+            opacity: 1
+        };
+ */
+
+        //Modify
+        var translate_start_small = {
+            width: this.sizeX,
+            height: this.sizeY_small,
+            opacity: 1
+        };
+
+
+        //Modify
+        var translate_start_medium = {
+            width: this.sizeX,
+            height: this.sizeY_medium,
             opacity: 1
         };
 
@@ -22,16 +40,19 @@
             opacity: 0
         };
 
+
         this.setTitle = function (title) {
             this.title = title;
-        }
+        };
 
-        this.error = function (title) {
+        this.log = function (title, position) {
             var intervalClose;
             var timeLimit = 5000;
             title = (this.title != '') ? this.title : title;
 
-            $('body').append(template(title, false));
+            var translate_start = (title.length > 30) ? translate_start_medium : translate_start_small;
+
+            $('body').append(template(title, 0));
             $('.alert-notify').animate(translate_start, this.timeIn, function () {
 
                 intervalClose = window.setTimeout(function () {
@@ -53,14 +74,17 @@
                     })
                 });
             });
-        }
+        };
 
         //Show alert when the call is correct
-        this.success = function (title) {
+        this.success = function (title, position) {
             var intervalClose;
             var timeLimit = 5000;
             title = (this.title != '') ? this.title : title;
-            $('body').append(template(title, true));
+
+            var translate_start = (title.length > 30) ? translate_start_medium : translate_start_small;
+
+            $('body').append(template(title, 1));
             $('.alert-notify').animate(translate_start, this.timeIn, function () {
 
                 intervalClose = window.setTimeout(function () {
@@ -82,18 +106,63 @@
                     })
                 });
             });
-        }
+        };
 
-        template = function (title, mode) {
+        this.error = function (title, position) {
+            var intervalClose;
+            var timeLimit = 5000;
+            title = (this.title != '') ? this.title : title;
+
+            var translate_start = (title.length > 30) ? translate_start_medium : translate_start_small;
+
+            $('body').append(template(title, 2));
+            $('.alert-notify').animate(translate_start, this.timeIn, function () {
+
+                intervalClose = window.setTimeout(function () {
+
+                    $('.alert-notify').animate(translate_end, this.timeOut, function () {
+                        //Eliminate element
+                        $('.alert-notify').remove();
+                        window.clearTimeout(intervalClose);
+                    })
+                }, timeLimit);
+
+
+                $(".alert-notify").click(function (e) {
+
+                    $('.alert-notify').animate(translate_end, this.timeOut, function () {
+
+                        $('.alert-notify').remove();
+                        window.clearTimeout(intervalClose);
+                    })
+                });
+            });
+        };
+
+
+
+
+
+        template = function (title, kind) {
             //Select style
+            var selected;
+            switch (kind) {
+                case 0:
+                    selected = "log-notify";
+                    break;
+                case 1:
+                    selected = "correct-notify";
+                    break;
+                case 2:
+                    selected = "incorrect-notify";
 
+            }
 
-            var selected = (mode) ? "correct-notify" : "incorrect-notify";
             var format = "<div class='" + selected + " alert-notify'>" +
                 "<h2 id='title'>" + title + "</h2>" +
               "</div>";
             return format;
         }
     }
-    alertify_tc = new Alert_tc();
-})(this)
+    alert_tc = new Alert_tc();
+})(this);
